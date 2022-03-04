90f819c09218fe477fae27e2d6fddaaa620a9b85
yadira <yadiraco304@gmail.com>
2022-03-04 12:47:37 -0500

    adding crud in articles and adding model comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,36 @@
 class ArticlesController < ApplicationController
-  def show
+  def home
     @article = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
   def create
-    
+    article_params = params[:article]
+    @article = Article.create(title: article_params[:title], content: article_params[:content], title_color: article_params[:title_color])
+    redirect_to "/"
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    article_params = params[:article]
+    @article.update(title: article_params[:title], content: article_params[:content], title_color: article_params[:title_color])
+    redirect_to "/"
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "articles#show"
+  root to: "articles#home"
 
-  get "/article/create", to: "articles#create"
+  get "/article/new", to: "articles#new"
+  post "articles", to: "articles#create"
+
+  get "/article/:id/edit", to: "articles#edit"
+  post "/article/:id", to:  "articles#update"
+
+  get "/articles", to: "articles#show"
+
+  delete "/article/:id", to: "articles#destroy"
 end
