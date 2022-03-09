ae082a3b1f1ba6241be49519a8611b65143e2668
yadira <yadiraco304@gmail.com>
2022-03-09 11:17:35 -0500

    adding styles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(user_name: params[:comment][:user_name], content: params[:comment][:content], article_id: params[:id])
-    # @article = Article.find(params[:id])
   end
 end
