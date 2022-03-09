40bfc14e7c993fa0855fbde6df38cd3d44157515
yadira <yadiraco304@gmail.com>
2022-03-09 12:23:11 -0500

    adding redirect when create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(user_name: params[:comment][:user_name], content: params[:comment][:content], article_id: params[:id])
+
+    redirect_to "/"
   end
 end
